666d318cc787119becb7fa73b587cf19c718f015
Chekoo <a316296241@163.com>
2017-04-07 16:37:27 +0800

    Chapter Seven

---
+++ flasky/config.py
@@ -0,0 +1,44 @@
+#coding=utf-8
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:   # 包含通用配置，子类分别定义专用的配置
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ubuntu'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASK_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASK_MAIL_SENDER = 'Flasky Admin <a316296241@gmail.com>'
+    FLASK_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):  # 参数为程序实例
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+# 注册了不同的配置环境
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
