e8924f226b2c950bd7129dc2307d0469ee67bcdc
Chekoo <a316296241@163.com>
2017-04-27 23:16:27 +0800

    update

--- flasky/config.py
+++ flasky/config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config: # 包含通用配置，子类分别定义专用的配置
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ubuntu'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess'
     SSL_DISABLE = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -15,8 +15,8 @@ class Config: # 包含通用配置，子类分别定义专用的配置
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <316296241@qq.com>'
-    FLASKY_ADMIN = 'ckenang@outlook.com' #('FLASKY_ADMIN')
+    FLASKY_MAIL_SENDER = 'Flasky Admin <xxxxxx@xx.com>'
+    FLASKY_ADMIN = 'xx@xx.com' #('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
